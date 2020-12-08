f315ad74862c1ba387f2639492a289a17be8126a
Aahan Sharma <sharmaaahan15@gmail.com>
2020-12-08 18:25:50 +0530

    added server files

---
+++ batteries.js
@@ -0,0 +1,22 @@
+var batteriesData = [
+  {
+    id: 1,
+    name: "5351",
+    brand: "Exide",
+    price: 500,
+  },
+  {
+    id: 2,
+    name: "2342",
+    brand: "Amaron",
+    price: 600,
+  },
+  {
+    id: 3,
+    name: "9878",
+    brand: "Exide",
+    price: 3191,
+  },
+];
+
+module.exports = batteriesData;
